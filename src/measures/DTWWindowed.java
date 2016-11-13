16c654a2e7d16723c793f70e9600b8c536d5b785
Francois Petitjean <francois.petitjean@monash.edu>
2016-11-14 09:26:12 +1100

    Bug fix calculation DTW with warping window

--- src/measures/DTWWindowed.java
+++ src/measures/DTWWindowed.java
@@ -44,7 +44,7 @@ public class DTWWindowed extends DTW{
 			int jStart = (i-windowSize<1)?1:i-windowSize;
 			int jStop = (i+windowSize+1>length2)?length2:i+windowSize+1;
 			
-			matrix[i][jStart-1] = Double.POSITIVE_INFINITY;
+			if(jStart!=1)matrix[i][jStart-1] = Double.POSITIVE_INFINITY;
 			for (j = jStart; j < jStop; j++) {
 				matrix[i][j] = min(matrix[i - 1][j - 1],matrix[i][j - 1], matrix[i - 1][j])
 						+ Tools.squaredDistance(series1[i],series2[j]);
